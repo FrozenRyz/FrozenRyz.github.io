bb92e7c062dbe037ffbaa5525982994e27c83ecc
FrozenRyz <87430244+FrozenRyz@users.noreply.github.com>
2021-12-04 17:20:12 +0200

    Delete main directory

--- main/assets/js/magic.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(()=> {
-    var isFirefox = typeof InstallTrigger !== 'undefined';
-    if (isFirefox) {
-        $('body').css('background', '#0c0c0c')
-    }
-})
--- main/assets/js/preloader.js
+++
@@ -1,6 +0,0 @@
-$(window).on('load', function () {
-    var $preloader = $('#page-preloader'),
-    $spinner   = $preloader.find('.spin');
-    $spinner.delay(100).fadeOut('slow');
-    $preloader.delay(450).fadeOut('slow');
-});
